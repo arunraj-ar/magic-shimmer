a471b489c186df88ad9ae21e296d74883cf7102a
arunraj-ar <mail.arunrajar@gmail.com>
2024-10-21 00:03:18 +0530

    getStyles method

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,40 +1,49 @@
-import React from 'react';
-import './Loader.css'; // Make sure this path is correct
+import React from "react";
+import "./Loader.css"; // Make sure this path is correct
 
-const Loader = ({ shape = 'rectangle', width = '100%', height = '20px', animate = true, speed = 2, theme = 'light', layout = [] }) => {
+const Loader = ({
+  shape = "rectangle",
+  width = "100%",
+  height = "20px",
+  animate = true,
+  speed = 2,
+  theme = "light",
+  layout = [],
+}) => {
   const getStyles = (item) => {
-    const {shape = 'rectangle'} = item
-    switch(shape){
-      case 'rectangle':
+    const { shape = "rectangle" } = item;
+    switch (shape) {
+      case "rectangle":
         return {
-          width: item.width || '100%',
-          height: item.height || '20px',
-        }
-      case 'circle':
+          width: item.width || "100%",
+          height: item.height || "20px",
+        };
+      case "circle":
         return {
-          width: item.width || '100%',
-          height: item.height || '20px',
-          borderRadius: '100%'
-        }
-  }
+          width: item.width || "100%",
+          height: item.height || "20px",
+          borderRadius: "100%",
+        };
+      default:
+        return {
+          width: item.width || "100%",
+          height: item.height || "20px",
+        };
+    }
+  };
 
   const layoutStructure = layout.map((item, index) => (
     <div
       key={index}
-      className={`hole ${item.shape} ${animate ? 'animate' : ''}`}
-      style={{
-        width: item.width || '100%',
-        height: item.height || '20px',
-        borderRadius: item.shape === 'circle' ? `100%`: '',
-        // backgroundColor: theme === 'dark' ? '#333' : '#f0f0f0',
-      }}
+      className={`hole ${item.shape} ${animate ? "animate" : ""}`}
+      style={getStyles(item)}
     />
   ));
 
   return (
-    <div className='back'>
-      <div className='base'>
-        {layoutStructure.length>0 && layoutStructure}
+    <div className="back">
+      <div className="base">
+        {layoutStructure.length > 0 && layoutStructure}
       </div>
     </div>
   );
