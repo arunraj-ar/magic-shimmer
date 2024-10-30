4484e40ee29e808b355869696a239be56a4b94c3
arunraj-ar <mail.arunrajar@gmail.com>
2024-10-30 23:50:56 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ function App() {
           layout={[
             { shape: "rectangle", width: "100%", height: "20px" },
             { shape: "rectangle", width: "50px", height: "50px" },
-            // { shape: "rectangle", width: "80%", height: "15px" },
-            // { shape: "rectangle", width: "100%", height: "100px" },
-            // { shape: "rectangle", width: "100px", height: "10rem" },
+            { shape: "rectangle", width: "80%", height: "15px" },
+            { shape: "rectangle", width: "100%", height: "100px" },
+            { shape: "rectangle", width: "100px", height: "10rem" },
           ]}
           speed={4}
           xAlign="center"
